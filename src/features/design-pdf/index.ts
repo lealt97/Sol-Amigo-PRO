d5487754fac2f23a448b80467bfe1a7943eda4d6
lealt97 <lealt97@gmail.com>
2026-07-09 14:29:05 -0300

    Add Design PDF feature entrypoint

---
+++ src/features/design-pdf/index.ts
@@ -0,0 +1,5 @@
+export { DesignPdfPage as DesignPdf } from './pages/DesignPdfPage';
+export { PdfPreview } from './components/PdfPreview';
+export { pdfDesignService } from './services/pdfDesignService';
+export { buildSvgTemplate } from './engines/svgTemplateEngine';
+export type * from './types/pdfDesignTypes';
