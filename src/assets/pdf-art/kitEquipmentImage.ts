bf2fc6fe7a805aa0402b1fb9929b041f49b3de86
lealt97 <lealt97@gmail.com>
2026-08-03 19:19:16 -0300

    feat: montar arte vetorizada do kit

---
+++ src/assets/pdf-art/kitEquipmentImage.ts
@@ -0,0 +1,8 @@
+import part0 from './kitEquipmentPart0';
+import part1 from './kitEquipmentPart1';
+import part2 from './kitEquipmentPart2';
+import part3 from './kitEquipmentPart3';
+
+export const kitEquipmentImage = 'data:image/png;base64,' + part0 + part1 + part2 + part3;
+
+export default kitEquipmentImage;
