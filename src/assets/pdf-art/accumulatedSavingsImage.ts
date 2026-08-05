62742a745f31ba2d649a045936e6ceb67c224be7
lealt97 <lealt97@gmail.com>
2026-08-04 23:42:35 -0300

    feat: montar arte de economia acumulada

---
+++ src/assets/pdf-art/accumulatedSavingsImage.ts
@@ -0,0 +1,9 @@
+import part0 from './accumulatedSavingsPart0';
+import part1 from './accumulatedSavingsPart1';
+import part2 from './accumulatedSavingsPart2';
+import part3 from './accumulatedSavingsPart3';
+import part4 from './accumulatedSavingsPart4';
+
+export const accumulatedSavingsImage = 'data:image/png;base64,' + part0 + part1 + part2 + part3 + part4;
+
+export default accumulatedSavingsImage;
