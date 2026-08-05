7839d9f49d3e2eac6a0fddc301eff14c2e20e6b1
lealt97 <lealt97@gmail.com>
2026-08-04 23:39:06 -0300

    feat: montar nova arte da linha do tempo

---
+++ src/assets/pdf-art/implementationTimelineNewImage.ts
@@ -0,0 +1,7 @@
+import part0 from './implementationTimelineNewPart0';
+import part1 from './implementationTimelineNewPart1';
+import part2 from './implementationTimelineNewPart2';
+
+export const implementationTimelineNewImage = 'data:image/png;base64,' + part0 + part1 + part2;
+
+export default implementationTimelineNewImage;
